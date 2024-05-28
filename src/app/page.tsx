569c98daee22a5fb4dc08eedc64cb1eb84d823df
ghazytp <ghazytp@gmail.com>
2024-05-28 13:16:13 +0700

    first commit & initialize project

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main>
+      <div className="bg-yellow-400 p-4">
+        <h1 className="text-6xl">Life</h1>
+        <h1 className="text-6xl">Count.</h1>
+        <h4>Time is limited, but you are not.</h4>
+      </div>
+    </main>
+  )
+}
